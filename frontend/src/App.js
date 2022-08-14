eadf90c2442f187f74edad510981f88208e2ea96
MonkeySki <rebeccafredericknz@gmail.com>
2022-08-15 10:51:44 +1200

    Submit Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 // import SEPractice from "./pages/SE-Practice";
 // import SubmitArticle from "./pages/Submit-Article"; 
 // import NotFoundPage from "./pages/404";
 const App = () =>  {
     return (
-        <Routes>
+      <BrowserRouter>
         <div>
-          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-            {/* <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
-            </ul> */}
-          <div className="content">
-            I am words
-          </div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+          <Routes>
+          <Route path='/' element={<Home></Home>} />
+          {/* <Route path='/create-book' element={<CreateBook></CreateBook>} />
+          <Route path='/edit-book/:id' element={<UpdateBookInfo></UpdateBookInfo>} />
+          <Route path='/show-book/:id' element={<ShowBookDetails></ShowBookDetails>} /> */}
+          </Routes>
         </div>
-        </Routes>
+      </BrowserRouter>
+        // <Routes>
+        // <div>
+        //   <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        //     {/* <ul className="header">
+        //         <li><a href = "/">Home</a></li>
+        //         <li><a href = "/SEPractice">Select the Practice</a></li>
+        //         <li><a href = "/SubmitArticle">Submit an Article</a></li>
+        //     </ul> */}
+        //   <div className="content">
+        //     I am words
+        //   </div>
+        // </div>
+        // </Routes>
     );
 }
  
